ae22a0113ebbe4031665ad7156edf4987ff1c7a6
Tanik.sam <tanik.sam@yandex.ru>
2020-08-30 19:45:24 +0300

    https://github.com/Tanik-sam/-FD3-Samoilovich.git

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -7,7 +7,7 @@
     var cG=[];
     for ( var a=0; a<this.props.columnG.length; a++ ) {
       var columnGood=this.props.columnG[a];
-      var cGs=React.DOM.span({className:'ColumnN'},columnGood.text);
+      var cGs=React.DOM.span({key:columnGood.code,className:'ColumnN'},columnGood.text);
       cG.push(cGs);
     }
     var rG=[];
