544a1352457902d4d3a301a26880fbedd4080554
Tanik.sam <tanik.sam@yandex.ru>
2020-08-30 10:36:16 +0300

    https://github.com/Tanik-sam/-FD3-Samoilovich.git

---
+++ ishop/ishop.js
@@ -0,0 +1,27 @@
+﻿var iShop = React.createClass({
+
+  displayName: 'iShop',
+
+  getDefaultProps: function() {
+    return { question: "Вопрос ни о чём" }
+  },
+
+  render: function() {
+
+    var answersCode=[];
+    for ( var a=0; a<this.props.answers.length; a++ ) {
+      var answer=this.props.answers[a];
+      var answerCode=        
+        React.DOM.div({key:answer.code,className:'Answer'},
+          React.DOM.span({className:'Count'},answer.count),
+          React.DOM.span({className:'Text'},answer.text),
+        );
+      answersCode.push(answerCode);
+    }
+    return React.DOM.div( {className:'iShop'}, 
+      React.DOM.div( {className:'Question'}, this.props.question ),
+      React.DOM.div( {className:'Answers'}, answersCode ),
+    );
+  },
+
+});
