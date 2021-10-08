b0e53ba3fdec95415675f50b5ae26e2bcab4e808
Tanik.sam <tanik.sam@yandex.ru>
2021-10-08 23:58:41 +0300

    tatiana

--- filter/SelectionBuild.js
+++ filter/SelectionBuild.js
@@ -13,8 +13,11 @@ var SelectionBuild = React.createClass({
       getInitialState: function() {
         return { 
                listArray:this.props.wordsArray,
+               countClick:0,
                }
       },
+
+      
       inputTextChanged: function(EO)
       {
            var x=EO.target.value;   
@@ -30,18 +33,29 @@ var SelectionBuild = React.createClass({
 
       function gg(){ console.log ('reкуку')}
     
-
+      
 return;
    },
+   checkBoxChecked:function(){
+
+    this.setState( (prevState, props) => { return {countClick:prevState.countClick+1}; } );
+     function compareWords(a,b){
+       if (a.text<b.text) return -1;
+       if (a.text>b.text) return 1;
+       return 0;
+     }
+     //this.setState({listArray:this.setState.listArray.sort()});
+    console.log('aga', this.state.listArray.sort(compareWords), 'countClick=',this.setState.countClick)
+  },
     render: function() {
  
-          var wordsSelection=this.listArray.map( v =>
+          var wordsSelection=this.state.listArray.map( v =>
               React.DOM.option({key:v.code,className:'Word'},v.text,
               )
             );
           return React.DOM.div( {className:'SelectionBuld'}, 
             React.DOM.div( {className:'WordText'}, this.props.word ),
-            React.DOM.input( {type:"checkbox"} ),
+            React.DOM.input( {type:"checkbox", defaultChecked:false, onChange: this.checkBoxChecked} ),
             React.DOM.input( {type:"text",defaultValue:"Введите текст", onChange:this.inputTextChanged} ),
             React.DOM.input( {value:'сброс',type:"button",onClick: this.makeDefault}),
             React.DOM.div({className:'SelBul'},
