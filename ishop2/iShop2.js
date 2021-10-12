8012b877d96bdc52c5db10f6b7345e6844d72746
Tanik.sam <tanik.sam@yandex.ru>
2021-10-12 21:45:23 +0300

    tatiana

---
+++ ishop2/iShop2.js
@@ -0,0 +1,37 @@
+﻿var iShop2 = React.createClass({
+
+  displayName: 'iShop2',
+
+
+
+  render: function() {
+    var b=1;
+    var cG=[];
+    for ( var a=0; a<this.props.columnG.length; a++ ) {
+      var columnGood=this.props.columnG[a];
+      var cGs=React.DOM.th({key:columnGood.code,className:'ColumnN'},columnGood.text);
+      cG.push(cGs);
+    }
+     
+     var stringSelect=this.props.rowG.map( v =>
+     React.createElement(iShopTr, {key:v.codeGood,
+        nameGood:v.nameGood, priceGood:v.priceGood, urlGood:v.urlGood, quantityGood:v.quantityGood})
+        )
+    
+
+
+
+
+     return React.DOM.div( {className:'iShop2'}, 
+      React.DOM.table({className:'tableIshop'}, 
+        React.DOM.caption( {className:'MarketName'}, this.props.marketName ),
+        React.DOM.tbody( {className:'tableBody'}, 
+         React.DOM.tr( {className:'ColumnName'}, cG ), 
+         stringSelect,
+                  ),
+      )
+      
+    );
+  },
+
+});
---
+++ ishop2/iShopTr.js
@@ -0,0 +1,27 @@
+﻿var iShopTr = React.createClass({
+
+  displayName: 'iShopTr',
+
+  deleteRow: function(stringNumber){
+    console.log("ты меня нажал", "я", stringNumber);
+  },
+
+  render: function() {
+
+    var rowGood=this.props.rowG;
+    console.log(this.props.rowNumber);
+   
+    return React.DOM.tr({key:this.props.codeGood,className:'Row'},
+           React.DOM.td({className:'RowN'},this.props.nameGood),
+           React.DOM.td({className:'RowN'},this.props.priceGood),
+           React.DOM.td({className:'RowN'},
+           React.DOM.img({className:'Img', src: this.props.urlGood, width:150, height:150}),
+           ),
+           React.DOM.td({className:'RowN'},this.props.quantityGood),
+           React.DOM.td({className:'RowN'},
+           React.DOM.input( {type:'button',value:'delete',onClick:this.deleteRow(this.props.codeGood)} ), ),
+           )
+         
+  },
+
+})
